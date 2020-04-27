c2ae960f8452bb852e0a0c1e6c73c83d8fbf7841
KlSchlitzohr <lasisa.kurz@gmx.de>
2020-04-27 16:39:11 +0200

    Debug Message Removed

--- src/de/klschlitzohr/stickfight/game/GameManager.java
+++ src/de/klschlitzohr/stickfight/game/GameManager.java
@@ -51,11 +51,8 @@ public class GameManager {
         if (playersInQueue.contains(player))
             return true;
             playersInQueue.add(player);
-        System.out.println("aaa" + player.getName());
             if (playersInQueue.size() == 2) {
-                System.out.println("aaaaaa");
                 for (Arena arena : avivableArenas) {
-                    System.out.println("aaasdasda");
                     if (arena.getPlayersinarena().size() == 0) {
                         for (Player playerinque : playersInQueue) {
                             joinArenaName(arena.getName(),playerinque);
